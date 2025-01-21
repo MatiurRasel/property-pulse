fe8022cd72f9a48a179c745585c98b69130955ee
Matiur Rasel <matiurrasel1002@gmail.com>
2025-01-21 21:03:37 +0600

    --

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -4,7 +4,7 @@ import { getSessionUser } from "@/utils/getSessionUser";
 import cloudinary from "@/config/cloudinary";
 
 
-
+export const dynamic = 'force-dynamic';
 
 //GET /api/properties
 export const GET = async (request) => {
